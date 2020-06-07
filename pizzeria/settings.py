b5851df1c6c49d47b7767d131f5dc0a2a7737b2c
Peter Marki <marpeter@protonmail.com>
2020-06-07 19:27:46 +0200

    Add Django REST Framework

--- pizzeria/settings.py
+++ pizzeria/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third party
+    'rest_framework',
+
+    # Own
     'ingredient',
     'order',
     'pizza',
